85a6448f753a8977fbba49f6e31459941b2d1e2f
feriel-bns <ferielmimi76@gmail.com>
2021-09-17 12:18:16 +0200

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,16 +1,16 @@
 <?php
 
 //Define your host here.
-$HostName = "'us-cdbr-east-04.cleardb.com'";
+$HostName = "nnsgluut5mye50or.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 
 //Define your database username here.
-$HostUser = "b4a84a4b9166f9";
+$HostUser = "r7iz5wrci1spczw2";
 
 //Define your database password here.
-$HostPass = "8e08be0f";
+$HostPass = "y1qnfwttrqsdtg20";
 
 //Define your database name here.
-$DatabaseName = "heroku_abde6316f609ed5";
+$DatabaseName = "zzjr2yzlkkznr5gg";
 
 $conn=mysqli_connect($HostName, $HostUser, $HostPass, $DatabaseName) or die("connection impossible");
 if ($conn){
