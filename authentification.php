cd3343df798cc41429620f28275cb1c799660345
feriel-bns <ferielmimi76@gmail.com>
2021-09-17 11:41:57 +0200

    Update authentification.php

--- authentification.php
+++ authentification.php
@@ -1,8 +1,8 @@
 <?php
 
 require_once 'conn.php';
-$user_name =$_GET['username'] ; //$valeur_pseudo = $_POST['pseudo'];// des variales perso = des varaible de base de donner                                        // $valeur_password = $_POST['password'];
-$user_pass = $_GET['password'];
+$user_name =$_POST['username'] ; //$valeur_pseudo = $_POST['pseudo'];// des variales perso = des varaible de base de donner                                        // $valeur_password = $_POST['password'];
+$user_pass = $_POSTT['password'];
 $mysql_query = "select username,password from compte where login like '$user_name' and  motdepasse like '$user_pass';";
 $result = mysqli_query($conn, $mysql_query);
 
